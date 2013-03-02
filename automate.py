8a5239c6aba58e318a7c87c9a8048672a9d3b62b
Mehyo <mehyo@Mehyo-pc.(none)>
2013-03-02 18:09:58 +0100

    First commit

---
+++ automate.py
@@ -0,0 +1,35 @@
+import automaton
+
+
+def completer(aut):
+	return aut
+
+def union(aut1, aut2):
+	return aut_union
+
+def intersection(aut1, aut2):
+	return aut_inter
+
+def miroir(aut):
+	return aut_mirror
+
+def determinisation(aut):
+	return aut_deter
+
+def complement(aut):
+	return aut_comp
+
+def minimiser(aut):
+	return aut_mini
+
+def expression_vers_automate(E):
+	return aut
+
+
+"Un au choix"
+
+def double_renversement():
+	return aut
+
+def analyseur(chaine):
+	return liste
