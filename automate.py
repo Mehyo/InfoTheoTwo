a1c5464f0983f52906e209f33c562007d4191456
ysenel <s.yasin33@hotmail.fr>
2013-03-19 16:12:45 +0100

    ajout fonction miroir

--- automate.py
+++ automate.py
@@ -11,6 +11,21 @@ def intersection(aut1, aut2):
 	return aut_inter
 
 def miroir(aut):
+	i = 0
+	new_transitions = []
+	transitions = list(aut.get_transitions())
+
+	while i < len(transitions):
+		new_transitions.append((transitions[i][2], transitions[i][1], transitions[i][0]))
+		i+=1
+
+	aut_mirror = automaton . automaton (
+	alphabet = aut.get_alphabet(),
+	epsilons = [ '0 '] ,
+	states = aut.get_states() , initials = aut.get_final_states() , finals = aut.get_initial_states(),
+	transitions = new_transitions
+	)
+
 	return aut_mirror
 
 def determinisation(aut):
@@ -33,3 +48,20 @@ def double_renversement():
 
 def analyseur(chaine):
 	return liste
+
+
+
+
+
+def main():
+	aut1 = automaton . automaton (
+	epsilons = [ '0 '] ,
+	states = [1] , initials = [0] , finals = [2] ,
+	transitions = [(0 , 'a ' ,1) , (1 , 'b ' ,2)]
+	)
+	aut_miroir = miroir(aut1)
+	aut1.display()
+	aut_miroir.display()
+
+
+main()
